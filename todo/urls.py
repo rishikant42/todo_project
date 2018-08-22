09241c16e4302cade5d34541ad932dfa6c24808f
rishikant42 <rksbtp@gmail.com>
2018-08-23 00:51:08 +0530

    small update in urls

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from views import TaskList, TaskDetail
 
 urlpatterns = [
-    url(r'^tasks/?', TaskList.as_view(), name='task_list_api'),
+    url(r'^tasks/?$', TaskList.as_view(), name='task_list_api'),
     url(r'^tasks/(?P<pk>[0-9]+)/$', TaskDetail.as_view(), name='task_detail_api'),
 ]
