2fd5ef0572d3396b0569c9461652308602700c61
rishikant42 <rksbtp@gmail.com>
2018-09-01 20:23:52 +0530

    specified static dir

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static/'
 
 CELERY_PROJECT_NAME = 'todo.clery_tasks'
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
